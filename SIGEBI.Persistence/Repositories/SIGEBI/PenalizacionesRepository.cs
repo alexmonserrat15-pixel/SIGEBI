a7854cfa2f57c8c50a7658a33eec328517d96a53
Yohanser <20242537@itla.edu.do>
2026-03-12 00:24:25 -0400

    New

--- SIGEBI.Persistence/Repositories/SIGEBI/PenalizacionesRepository.cs
+++ SIGEBI.Persistence/Repositories/SIGEBI/PenalizacionesRepository.cs
@@ -1,10 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using SIGEBI.Domain.Base;
+using SIGEBI.Domain.Entities;
+using SIGEBI.Domain.Entities.SIGEBI;
+using SIGEBI.Domain.Interfaces.Repositories;
+using SIGEBI.Domain.Repository;
+using SIGEBI.Domain.Repository.SIGEBI;
+using SIGEBI.Infrastructure.Persistence;
 
-namespace SIGEBI.Persistence.Repositories.SIGEBI
+namespace SIGEBI.Infrastructure.Persistence.Repositories
 {
-    internal class PenalizacionesRepository
+    public class PenalizacionRepository : IPenalizacionesRepository
     {
+        private readonly SIGEBIDbContext _context;
+
+        public PenalizacionRepository(SIGEBIDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Penalizaciones>> GetAllAsync()
+        {
+            return await _context.Penalizaciones.ToListAsync();
+        }
+
+        public async Task<Penalizaciones?> GetByIdAsync(int id)
+        {
+            return await _context.Penalizaciones.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Penalizaciones>> GetByUsuarioIdAsync(int usuarioId)
+        {
+            return await _context.Penalizaciones
+                .Where(p => p.IdUsuario == usuarioId)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Penalizaciones penalizacion)
+        {
+            await _context.Penalizaciones.AddAsync(penalizacion);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Penalizaciones penalizacion)
+        {
+            _context.Penalizaciones.Update(penalizacion);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var penalizacion = await _context.Penalizaciones.FindAsync(id);
+
+            if (penalizacion != null)
+            {
+                _context.Penalizaciones.Remove(penalizacion);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        Task<IEnumerable<Penalizaciones>> IPenalizacionesRepository.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<Penalizaciones>> IPenalizacionesRepository.GetByUsuarioIdAsync(int usuarioId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Penalizaciones?> IPenalizacionesRepository.GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(Penalizaciones penalizacion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Penalizaciones penalizacion)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- SIGEBI.Persistence/Repositories/SIGEBI/PrestamosRepository.cs
+++ SIGEBI.Persistence/Repositories/SIGEBI/PrestamosRepository.cs
@@ -1,10 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using SIGEBI.Domain.Base;
+using SIGEBI.Domain.Entities;
+using SIGEBI.Domain.Entities.SIGEBI;
+using SIGEBI.Domain.Interfaces.Repositories;
+using SIGEBI.Domain.Repository;
+using SIGEBI.Domain.Repository.SIGEBI;
+using SIGEBI.Infrastructure.Persistence;
 
-namespace SIGEBI.Persistence.Repositories.SIGEBI
+namespace SIGEBI.Infrastructure.Persistence.Repositories
 {
-    internal class PrestamosRepository
+    public class PrestamoRepository : IPrestamoRepository
     {
+        private readonly SIGEBIDbContext _context;
+
+        public PrestamoRepository(SIGEBIDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Prestamos>> GetAllAsync()
+        {
+            return await _context.Prestamos
+                .Include(p => p.Usuario)
+                .Include(p => p.GestionLibros)
+                .ToListAsync();
+        }
+
+        public async Task<Prestamos?> GetByIdAsync(int id)
+        {
+            return await _context.Prestamos
+                .Include(p => p.Usuario)
+                .Include(p => p.GestionLibros)
+                .FirstOrDefaultAsync(p => p.IdPrestamo == id);
+        }
+
+        public async Task<IEnumerable<Prestamos>> GetPrestamosActivosAsync()
+        {
+            return await _context.Prestamos
+                .Where(p => p.Estado == "Activo")
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Prestamos>> GetPrestamosByUsuarioAsync(int usuarioId)
+        {
+            return await _context.Prestamos
+                .Where(p => p.IdUsuario == usuarioId)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Prestamos prestamo)
+        {
+            await _context.Prestamos.AddAsync(prestamo);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Prestamos prestamo)
+        {
+            _context.Prestamos.Update(prestamo);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var prestamo = await _context.Prestamos.FindAsync(id);
+
+            if (prestamo != null)
+            {
+                _context.Prestamos.Remove(prestamo);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        Task<IEnumerable<Prestamos>> IPrestamoRepository.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Prestamos?> IPrestamoRepository.GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Prestamos>> GetPrestamosByUsuario(int usuarioId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Prestamos>> GetPrestamosActivos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(Prestamos prestamo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Prestamos prestamo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
