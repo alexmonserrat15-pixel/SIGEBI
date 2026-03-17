b4e4ce63fc77a2d5d4592240fe5d39843705ebf4
Yohanser <20242537@itla.edu.do>
2026-03-16 23:11:20 -0400

    New

---
+++ SIGEBI.Application/Interfaces/IUsuarioService.cs
@@ -0,0 +1,21 @@
+﻿using SIGEBI.Application.Dtos;
+using SIGEBI.Application.Dtos.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIGEBI.Application.Interfaces
+{
+    public interface IUsuarioService
+    {
+        Task<IEnumerable<UsuarioDto>> GetAllUsuarios();
+
+        Task<UsuarioDto?> GetUsuarioById(int id);
+
+        Task CrearUsuario(CreateUsuarioDto dto);
+
+        Task ActualizarUsuario(UpdateUsuarioDto dto);
+
+        Task EliminarUsuario(DeleteUsuarioDto dto);
+    }
+}
