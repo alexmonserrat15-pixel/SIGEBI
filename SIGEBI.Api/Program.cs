d2cb442dcf64b649bffd66852ccbad8aa4d2ee08
Yohanser <20242537@itla.edu.do>
2026-03-28 04:07:28 -0400

    New

--- SIGEBI.Api/Program.cs
+++ SIGEBI.Api/Program.cs
@@ -44,6 +44,9 @@ namespace SIGEBI.Api
 
             var app = builder.Build();
 
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
