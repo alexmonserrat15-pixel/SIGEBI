ed3db1f38dbcfa9c51cbbae25f75f381ea50477d
Yohanser <20242537@itla.edu.do>
2026-04-07 23:11:40 -0400

    New

--- SIGEBI.Web/Program.cs
+++ SIGEBI.Web/Program.cs
@@ -3,14 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
-
 builder.Services.AddHttpClient("SIGEBI_API", client =>
 {
-    client.BaseAddress = new Uri("https://localhost:5170/"); 
+    client.BaseAddress = new Uri("http://localhost:5170/");
 });
 
 builder.Services.AddScoped<UsuarioService>();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -25,8 +25,6 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Usuario}/{action=Index}/{id?}");
 
 app.Run();
